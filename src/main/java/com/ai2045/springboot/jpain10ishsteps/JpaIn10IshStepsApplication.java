52736791b914dfea2e1e7fc86c679b9026868488
ai1045proscensus <agca.software@gmail.com>
2022-11-13 20:40:53 +0300

    Initial commit
    project neu generiert mit spring initializr

---
+++ src/main/java/com/ai2045/springboot/jpain10ishsteps/JpaIn10IshStepsApplication.java
@@ -0,0 +1,13 @@
+package com.ai2045.springboot.jpain10ishsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIn10IshStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaIn10IshStepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai2045/springboot/jpain10ishsteps/JpaIn10IshStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai2045.springboot.jpain10ishsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaIn10IshStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
